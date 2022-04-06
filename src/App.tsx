92259d6512d64a42270a4e7962fc4c30c8f04605
Kayo Oliveira <35928107+kayooliveira@users.noreply.github.com>
2022-04-06 20:58:18 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Routes from './Services/Routes'
+/**
+ *
+ * @version 0.0.1
+ *
+ * @author Kayo Oliveira
+ *
+ * Description: App component
+ *
+ */
+
+const App: React.FC = () => {
+    return <Routes />
+}
+
+export default App
