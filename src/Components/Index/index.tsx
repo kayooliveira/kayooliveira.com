e6dac9bfe058cc93abddb55e63d3f6cb197ee66d
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-04-12 21:43:44 -0300

    Creating index component and styles for it

--- src/Components/Index/index.tsx
+++ src/Components/Index/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import * as C from './styles'
-
+import logo from '../../Assets/img/logo.png'
+import { Text } from '../../UI/Text'
+import { FaCaretDown, FaChevronDown } from 'react-icons/fa'
 const Index: React.FC = () => {
     return (
         <>
             <C.IndexContainer>
                 <C.IndexContent>
-                    <C.IndexTitle>Kayo Oliveira</C.IndexTitle>
-                    <C.IndexDescription>Desenvolvedor Web</C.IndexDescription>
+                    <C.IndexLogo alt="logo" src={logo} />
+                    <Text
+                        font="logo"
+                        color="white"
+                        transform="uppercase"
+                        size="h3"
+                    >
+                        Kayo Oliveira
+                    </Text>
                 </C.IndexContent>
+                <C.ArrowsDiv href="#main">
+                    <FaChevronDown />
+                    <FaChevronDown />
+                    <FaChevronDown />
+                </C.ArrowsDiv>
             </C.IndexContainer>
         </>
     )
--- src/Components/Index/styles.ts
+++ src/Components/Index/styles.ts
@@ -1,30 +1,78 @@
 import { styled } from '../../Services/Styled'
-
+import { keyframes } from '@stitches/react'
+import bgLarge from '../../Assets/img/bglarge.png'
 export const IndexContainer = styled('div', {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     width: '100vw',
-    height: '100vh',
+    minHeight: '100vh',
+    height: '100%',
+    maxWidth: '100vw',
+    overflow: 'hidden',
+    padding: '1rem',
     flexDirection: 'column',
-    backgroundColor: '$background'
+    backgroundImage: `url(${bgLarge})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'bottom',
+    backgroundRepeat: 'no-repeat',
+    '@lg': {
+        backgroundPosition: 'center'
+    }
 })
 
 export const IndexContent = styled('div', {
     display: 'flex',
     flexDirection: 'column',
     alignContent: 'center',
-    justifyContent: 'center'
-})
-
-export const IndexTitle = styled('h1', {
-    color: '#fff',
-    fontSize: '4rem',
-    margin: 0
-})
-
-export const IndexDescription = styled('p', {
-    color: '#4d4d4d',
+    justifyContent: 'center',
     alignSelf: 'flex-end',
-    marginTop: '-0.5rem'
+    maxWidth: '100%',
+    '& > span': {
+        maxWidth: '8ch',
+        '@md': {
+            maxWidth: 'unset',
+            fontSize: '3rem'
+        }
+    },
+    '@md': {
+        alignSelf: 'center'
+    }
+})
+
+export const IndexLogo = styled('img', {
+    width: '7.75rem',
+    '@md': {
+        alignSelf: 'center',
+        width: '10rem'
+    }
+})
+
+const bounce = keyframes({
+    '0%': {
+        transform: 'translateY(0)'
+    },
+    '50%': {
+        transform: 'translateY(-0.5rem)'
+    },
+    '100%': {
+        transform: 'translateY(0)'
+    }
+})
+
+export const ArrowsDiv = styled('a', {
+    position: 'absolute',
+    widtth: '100%',
+    bottom: '1rem',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    filter: 'drop-shadow(0px 0px 15px #fff)',
+    animation: `${bounce} 1s ease-in-out infinite`,
+    '& > svg': {
+        marginTop: '-2.5rem',
+        fill: '$white',
+        fontSize: '4rem'
+    }
 })
